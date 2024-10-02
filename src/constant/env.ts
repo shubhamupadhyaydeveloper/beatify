3b45111a21787f9f081950c5130e430cb27995b3
shubham upadhyay <shubhamwork48@gmail.com>
2024-10-02 18:14:13 +0530

    google auth complete and remove client secret

--- src/constant/env.ts
+++
@@ -1,6 +0,0 @@
-
-const GoggleclientId =
-  '258676774788-5k7ipu9prkpjuv6jdmgq5nohfd3i4e3b.apps.googleusercontent.com';
-const Gogglesecret = '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';
-const androidClient =
-  '258676774788-htk4vjvkgeh7q42poo22do24tu951bm8.apps.googleusercontent.com';
